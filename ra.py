0073e5b4816ab5a3de2a55c1a051f1ea053b5025
shisak66 <niteshkumar963001@gmail.com>
2025-12-27 16:39:45 +0530

    Change BOT_TOKEN in ra.py
    Updated BOT_TOKEN to a new value.

--- ra.py
+++ ra.py
@@ -9,7 +9,7 @@ from datetime import datetime, timezone
 from sseclient import SSEClient
 
 # ---------------- CONFIG ----------------
-BOT_TOKEN = "8500767077:AAGtncT-zc4ttcxjnbI1uJjpHBCSDyOEVcg"
+BOT_TOKEN = "8500767077:AAFHnkBPMgsjBQNeoKXrhvyJPuXIQ0zzEJE"
 
 if not BOT_TOKEN or BOT_TOKEN.strip() == "":
     print("❌ BOT_TOKEN missing inside ra.py file!")
